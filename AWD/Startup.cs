0ed5a08962490e674bf345c2a9f1ddf78716f8e8
rkaboobacker <rkaboobacker@suyati.com>
2014-12-12 20:15:59 +0530

    Changed

---
+++ AWD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AWD.Startup))]
+namespace AWD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- SampleAWD/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sample_AWD.MvcApplication" Language="C#" %>
